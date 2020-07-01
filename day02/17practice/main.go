51df79c66efecbbf55ff306e5f6016b04ffb0f2a
rhw943359178 <qiant@bsoft.com.cn>
2020-07-01 17:18:39 +0800

    回文字符串练习

--- day02/17practice/main.go
+++ day02/17practice/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -21,12 +22,62 @@ import (
 //}
 
 func main() {
-	test1()
+	//test1()
+	test2()
 }
 
 func test1() {
 	str := "how do you do how beautiful you are how are you"
 	s := strings.Split(str, " ")
 	fmt.Println(s)
-	fmt.Printf("%T", s)
+	//fmt.Printf("%T", s)
+	//	思路：把放进 map 中
+
+	strMap := make(map[string]int, 11)
+	fmt.Println(strMap)
+
+	for _, v := range s {
+		//fmt.Println(v)
+		_, ok := strMap[v]
+		if !ok {
+			strMap[v] = 1
+		} else {
+			strMap[v] += 1
+		}
+	}
+	//fmt.Println(strMap)
+	//	遍历 map
+	for k, v := range strMap {
+		fmt.Printf("%v=%d\n", k, v)
+	}
 }
+
+func test2() {
+	//	回文判断
+	//	字符串从右至左和从左至右读是一样的，这就是回文
+	//	上海自来水来自海上			山西运煤车煤运山西
+
+	//	方式一：正序和倒叙循环比较字符串，如果相同，则是回文(通过切片比较)
+	str := "上海自来水来自海上"
+
+	strSlice := make([]string, 0, 10)
+	lSlice := make([]string, 0, 10)
+	rSlice := make([]string, 0, 10)
+
+	for _, v := range str {
+		strSlice = append(strSlice, string(v))
+	}
+	for i := 0; i < len(strSlice); i++ {
+		lSlice = append(lSlice, strSlice[i])
+	}
+	for i := len(strSlice) - 1; i >= 0; i-- {
+		rSlice = append(rSlice, strSlice[i])
+	}
+
+	if reflect.DeepEqual(lSlice, rSlice) {
+		fmt.Println("是回文字符串")
+	} else {
+		fmt.Println("不是回文字符串")
+	}
+}
+
